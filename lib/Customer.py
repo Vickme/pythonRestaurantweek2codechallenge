a43d7144864a246f3eeba88292b00d817255aa48
victor <victor.njutha@student.moringaschool.com>
2023-12-17 08:57:49 +0300

    added reviewfunction

--- lib/Customer.py
+++ lib/Customer.py
@@ -8,3 +8,6 @@ Customer2 = Customer("Cedric", "Njutha")
 Restaurant1 = Restaurant("KFC")
 Restaurant2 = Restaurant("Pizzahut")
 
+Review1 = Review(Customer1, Restaurant1, 10)
+Review2 = Review(Customer2, Restaurant2, 6)
+
