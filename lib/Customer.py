d429c1bdc09f8a4d89c5fef815197e65a055b73c
victor <victor.njutha@student.moringaschool.com>
2023-12-17 08:55:19 +0300

    added restaurant function

--- lib/Customer.py
+++ lib/Customer.py
@@ -4,3 +4,7 @@ from Review import Review
 
 Customer1 = Customer("victor", "Njutha")
 Customer2 = Customer("Cedric", "Njutha")
+
+Restaurant1 = Restaurant("KFC")
+Restaurant2 = Restaurant("Pizzahut")
+
